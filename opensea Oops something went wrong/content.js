f3660fb7684f1c3925ed167cb2168d9c41b055a7
karakule3dfx <103366046+karakule3dfx@users.noreply.github.com>
2022-06-05 14:36:32 +0300

    Update content.js

--- opensea Oops something went wrong/content.js
+++ opensea Oops something went wrong/content.js
@@ -13,8 +13,24 @@ chrome.runtime.onMessage.addListener(
 				const targetNode = getElementByXpath('//h1[text()="Oops, something went wrong"]');
 				if(targetNode != null){
 					clearInterval(timerId); clearTimeout(myTimeout); girdi= false; 
-					
 					setTimeout(() => {window.history.back();}, 2000);
+				}
+				const problemNode = getElementByXpath('//pre[contains(@style,"word-wrap")]');
+				if(problemNode != null){
+					clearInterval(timerId); clearTimeout(myTimeout); girdi= false; 
+					setTimeout(() => {window.location.href=window.location.href;}, 2000);
+				}
+				const errorNode = getElementByXpath('//span[text()="Error adding your item: Failed to fetch"]');
+				const AllmostNode = getElementByXpath('//h4[text()="Almost done"]');
+				if(errorNode != null && AllmostNode == null){
+					const createButtonNode = getElementByXpath('//*[@id="__next"]/div[1]/main/div/div/section/div[2]/form/div/div[1]/span/button');
+					clearInterval(timerId); clearTimeout(myTimeout); girdi= false; 
+					setTimeout(() => {createButtonNode.click();}, 4000);
+				}
+				const pageIsLostNode = getElementByXpath('//h1[text()="This page is lost."]');
+				if(pageIsLostNode != null){
+					clearInterval(timerId); clearTimeout(myTimeout); girdi= false; 
+					setTimeout(() => {window.location.href=window.location.href;}, 4000);
 				}
 			 }
 			const myTimeout = setTimeout(() => { clearInterval(timerId); girdi= false;}, 120000);
@@ -27,24 +43,3 @@ function getElementByXpath(path) {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 //
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-
-
